9c14f228464ebdd05142eb528d51c1b5814a4196
Sky DeGrate <skydegrate@gmail.com>
2022-10-03 21:24:51 -0400

    Finalized main set up !

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,6 +23,273 @@ return (
     </div>
     <div id="Boards-wrapper">
 
+        <div
+        style={{marginLeft:"350px", width:"220px"}}
+        >
+        <img src="./boards1.png"/>
+
+        <div id="boards-dropdown-container"
+        style={{paddingTop:"30px", paddingLeft:"20px", display:"flex"}}
+        >
+
+       {/* Boards, japanese culture */}
+        <div className="dropdown is-hoverable">
+             <div class="dropdown-trigger">
+            
+            <button 
+            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px", marginRight:"15px"}}
+            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <strong
+             style={{fontSize: "35px"}}
+            >Japanese Culture</strong>
+            </button>
+      </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Anime & Manga
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Memes
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        All Things Kawaii
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Anime Wallpaper 
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Cosplay
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Vtubers
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+                <div></div>
+                {/* Boards, Video Games */}
+                <div className="dropdown is-hoverable">
+                            <div class="dropdown-trigger">
+                            
+                            <button 
+                            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px", marginRight:"15px"}}
+                            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <strong
+                            style={{fontSize: "35px"}}
+                            >Video Games</strong>
+                            </button>
+                    </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Multiplayer
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Pokemon
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Smash
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        League of Legends
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Minecraft
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Valorant/Fortnite/Overwatch
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Retro Games
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Misc. 
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+                <div></div>
+                {/* Boards, Interests */}
+                <div className="dropdown is-hoverable">
+                            <div class="dropdown-trigger">
+                            
+                            <button 
+                            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px" , marginRight:"15px"}}
+                            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <strong
+                            style={{fontSize: "35px"}}
+                            >Interests</strong>
+                            </button>
+                    </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Binge Watchers: TV & Movies 
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        True Crime
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Awww Animals
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Boxing/MMA
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Unboxing videos
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        ASMR Club
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Coding Friends
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Random 
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+                <div></div>
+
+                {/* Boards, Creative */}
+                <div className="dropdown is-hoverable">
+                            <div class="dropdown-trigger">
+                            
+                            <button 
+                            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px" , marginRight:"15px"}}
+                            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <strong
+                            style={{fontSize: "35px"}}
+                            >Creative</strong>
+                            </button>
+                    </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Cooking
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Literature
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        DIY Music
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Fashion
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        TikTok
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Graphic Design
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Artwork
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Misc. 
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+
+                <div></div>
+                {/* Boards, Other */}
+                <div className="dropdown is-hoverable">
+                            <div class="dropdown-trigger">
+                            
+                            <button 
+                            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px" , marginRight:"15px"}}
+                            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <strong
+                            style={{fontSize: "35px"}}
+                            >Other</strong>
+                            </button>
+                    </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Business
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Personal Finance Memes
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Current News
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Fitness
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Self Care
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Paranormal/Ghosts/Aliens
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+
+                <div></div>
+                {/* Boards, Music */}
+                <div className="dropdown is-hoverable">
+                            <div class="dropdown-trigger">
+                            
+                            <button 
+                            style={{paddingTop:"35px", paddingBottom:"35px", paddingLeft:"15px", marginRight:"15px"}}
+                            class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <strong
+                            style={{fontSize: "35px"}}
+                            >Music</strong>
+                            </button>
+                    </div>
+                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Hip-Hop
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Jazz
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Underground
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Hyperpop
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Top 40
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Indie/Emo/Punk
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Rock/Metal
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        EDM
+                        </a>
+                        </div>
+                        </div>
+                    </div>
+                {/* Board, end */}
+
+
+        </div>
+
+
+
+
+
+        </div>
+
     </div>
 
 </div>
