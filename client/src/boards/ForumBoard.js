e27405fa5eda0b74bb79257d14aa232ef73eb536
Sky DeGrate <skydegrate@gmail.com>
2022-10-04 22:48:19 -0400

    Need to figure out how to map sample post previews

--- client/src/boards/ForumBoard.js
+++ client/src/boards/ForumBoard.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import'../medium.ttf';
 import'../bold.ttf';
 import'../regular.ttf';
+import FormPosts from "./ForumPosts";
 
 const ForumBoard = () => {
 
@@ -24,67 +25,72 @@ style={{display: "flex", justifyContent: "center", position: "relative",
 
  }}>
 
-<div id="board-nav-links-wrapper"
+<div id="board-wrapper-3"
 style={{
     
     height: "50px", width:"850px",
-     paddingTop:"5,6px", borderRadius:"4px", left:"770px",
+     paddingTop:"6px", borderRadius:"4px", left:"770px",
     position:"absolute", top:"262px"
 
  }}>
     <div style={{
 
     height: "50px", width:"691px",
-    backgroundColor: "white", paddingTop:"6px", borderRadius:"4px", left:"482px",
-    position:"absolute", top:"18px"
+    backgroundColor: "white", paddingTop:"6px", borderRadius:"4px", left:"881px",
+    position:"absolute", top:"-129px"
     }}>
 
         
     </div>
 
     <img 
-    style={{position:"absolute", top:"14xpx", right:"390px", width:"60px", display:"inline-block", transform: "rotate(270deg)"}}
+    style={{position:"absolute", top:"-131px", right:"-12px", width:"60px", display:"inline-block", transform: "rotate(270deg)"}}
     
     src="./doublearrow.png"></img>
 
     
 <br/>
 <div id="board-title">
-<div style={{
-    display:"flex", justifyContent:"center",
-    position:"absolute", zIndex:"4", top:"-20px", right:"720px",
-     width:"456px",
-     paddingTop:"20px", paddingBottom:"20px", borderRadius:"10px",
-    backgroundColor: "black", border:"white 3px solid",
-    fontFamily:"bold-font",
-    fontSize:"35px",
-    color:"white",
-   
-    
-    }}>
- 
- Anime & Manga
+            <div style={{
+                display:"flex", justifyContent:"center",
+                position:"absolute", zIndex:"4", top:"-174px", right:"720px",
+                width:"456px",
+                paddingTop:"20px", paddingBottom:"20px", borderRadius:"10px",
+                backgroundColor: "black", border:"white 3px solid",
+                fontFamily:"bold-font",
+                fontSize:"35px",
+                color:"white",
+            
+                
+                }}>
+            
+            Anime & Manga
+
+            </div>
+
+
+
 
 </div>
 
+            <div id="board-background"
+            style={{
+                position:"absolute", top:"-39px", right:"-727px",
+                height: "60px", width:"1900px", paddingTop:"1000px", borderRadius:"4px",
+                backgroundColor: "white",
+                
+                }}>
+                
+
+            </div>
+                        <div style={{position:"absolute", left:"-288px", display: "grid", flex:"row wrap" }}>
+
+                        <div>
+                            <FormPosts />
+                        </div>
 
 
-
-</div>
-
-<div id="board-wrapper"
-style={{ display:"flex", display:"inline-block",}}
->
-<div id="board-posts">
-<div style={{
-    position:"absolute", top:"100px", right:"-327px",
-    height: "60px", width:"1500px", paddingTop:"700px", borderRadius:"4px",
-    backgroundColor: "white"
-
- }}>
-
-</div>
-
+                        </div>  
 <div id="board-comments">
 
 <div id="board-likes"></div>
@@ -95,8 +101,8 @@ style={{ display:"flex", display:"inline-block",}}
 
 </div>
 </div>
-</div>
-</div>
+
+
 
 </>
 
---
+++ client/src/boards/ForumPosts.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import'../medium.ttf';
+import'../bold.ttf';
+import'../regular.ttf';
+
+const FormPosts = () => {
+
+return (
+
+
+
+<>
+
+<div
+style={{
+    fontFamily:"bold-font", 
+    fontSize:"25px",
+    position:"absolute",
+    paddingTop:"20px", 
+    paddingLeft:"20px",
+    width:"500px",
+    backgroundColor:"blue", 
+    height:"200px",
+    border: "black 4px solid", 
+    borderRadius:"6px", 
+    display:"flex"
+    
+    }}
+>
+
+
+lkjdls
+</div>
+
+
+
+
+
+
+</>
+
+
+)}
+export default FormPosts
