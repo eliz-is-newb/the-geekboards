6037288557baec4b02ac7eec694ce445420f8618
Tsering Tsering <dtsering2@gmail.com>
2022-10-06 16:51:34 -0400

    password encrypted and fully functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_194728) do
+ActiveRecord::Schema.define(version: 2022_10_06_011950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,17 +39,10 @@ ActiveRecord::Schema.define(version: 2022_10_06_194728) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "userboardjoiners", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "board_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
